4a83d2defa44c8abbeb6ece909c3e841e0dda299
Lasse Dissing <me@lassedissing.com>
2023-10-27 07:11:01 +0200

    Draw walls and divider

--- pong/src/main.c
+++ pong/src/main.c
@@ -17,6 +17,13 @@ typedef struct {
     HMM_Vec2 position;
 } Vertex;
 
+typedef struct {
+    HMM_Vec2 min;
+    HMM_Vec2 max;
+} AABB;
+
+#define NUM_WALLS 2
+
 static struct {
     sg_pass_action pass_action;
 
@@ -25,6 +32,9 @@ static struct {
     uint num_vertices;
     sg_buffer vbo;
     sg_pipeline pipeline;
+
+    // World
+    AABB walls[NUM_WALLS];
 
 } self;
 
@@ -36,14 +46,20 @@ Vertex* allocate_vertices(uint num) {
     return (Vertex*) p;
 }
 
-static void init(void) {
+static void game_init(void) {
+    const float WALL_THICKNESS = 0.1;
+    self.walls[0] = (AABB) { { -1, -1 }, { 1, -1 + WALL_THICKNESS } };
+    self.walls[1] = (AABB) { { -1, 1 - WALL_THICKNESS }, { 1, 1 } };
+}
+
+static void render_init(void) {
     sg_setup(&(sg_desc) {
         .context = sapp_sgcontext(),
         .logger.func = slog_func,
     });
     self.pass_action = (sg_pass_action) { .colors[0] = {
                                               .load_action = SG_LOADACTION_CLEAR,
-                                              .clear_value = { 1.0, 0.0, 0.0, 1.0 },
+                                              .clear_value = { 0.0, 0.0, 0.0, 1.0 },
                                           } };
 
     // Setup quad renderer
@@ -65,7 +81,17 @@ static void init(void) {
     });
 }
 
-void draw_quad(HMM_Vec2 center, HMM_Vec2 extent) {
+static void draw_quad_vertices(HMM_Vec2 ps[4]) {
+    Vertex* v = allocate_vertices(6);
+    v[0].position = ps[0];
+    v[1].position = ps[1];
+    v[2].position = ps[3];
+    v[3].position = ps[1];
+    v[4].position = ps[2];
+    v[5].position = ps[3];
+}
+
+static void draw_quad(HMM_Vec2 center, HMM_Vec2 extent) {
     HMM_Vec2 ps[4] = {
         HMM_SubV2(center, extent),
         HMM_AddV2(center, (HMM_Vec2) { -extent.X, extent.Y }),
@@ -81,19 +107,38 @@ void draw_quad(HMM_Vec2 center, HMM_Vec2 extent) {
     v[5].position = ps[3];
 }
 
+static void draw_dashed_line(HMM_Vec2 start, HMM_Vec2 end, uint num_segments, f32 width) {
+    HMM_Vec2 d = HMM_SubV2(end, start);
+    HMM_Vec2 wd = HMM_MulV2F(HMM_NormV2((HMM_Vec2) { d.Y, -d.X }), width);
+
+    f32 segment_size = 1.0 / (2 * num_segments - 1);
+
+    for (uint i = 0; i < num_segments; ++i) {
+        f32 t = 2 * i * segment_size;
+        HMM_Vec2 segment_start = HMM_AddV2(start, HMM_MulV2F(d, t));
+        HMM_Vec2 segment_end = HMM_AddV2(start, HMM_MulV2F(d, t + segment_size));
+        HMM_Vec2 ps[4] = {
+            HMM_SubV2(segment_start, wd),
+            HMM_AddV2(segment_start, wd),
+            HMM_AddV2(segment_end, wd),
+            HMM_SubV2(segment_end, wd),
+        };
+        draw_quad_vertices(ps);
+    }
+}
+
+static void draw_aabb(AABB aabb) {
+    HMM_Vec2 center = HMM_DivV2F(HMM_AddV2(aabb.min, aabb.max), 2);
+    HMM_Vec2 extent = HMM_SubV2(aabb.max, center);
+    draw_quad(center, extent);
+}
+
 static void extract(void) {
-    static float t = 0;
-
-    float f1 = sinf(t);
-    float f2 = cosf(t);
-    float f3 = sinf(0.5 * t);
-
     self.num_vertices = 0;
-    draw_quad((HMM_Vec2) { 0.5 * f1, 0.5 * f2 }, (HMM_Vec2) { 0.1, 0.1 });
-    draw_quad((HMM_Vec2) { 0.0, 0.5 * f3 }, (HMM_Vec2) { 0.1, 0.3 });
-    draw_quad((HMM_Vec2) { -0.5 * f1, -0.5 }, (HMM_Vec2) { 0.2, 0.1 * f3 });
 
-    t += 0.1;
+    for (uint i = 0; i < NUM_WALLS; ++i) draw_aabb(self.walls[i]);
+
+    draw_dashed_line((HMM_Vec2) { 0.0, 0.88 }, (HMM_Vec2) { 0.0, -0.88 }, 32, 0.01);
 }
 
 static void submit(void) {
@@ -112,6 +157,11 @@ static void submit(void) {
 
     sg_end_pass();
     sg_commit();
+}
+
+static void init(void) {
+    game_init();
+    render_init();
 }
 
 static void frame(void) {
